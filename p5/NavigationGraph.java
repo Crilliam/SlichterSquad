23860b9194db621c5d2edff97ef124f87952fc21
Crilliam <willmustari@gmail.com>
2017-04-26 19:56:57 -0500

    Pile of Shit

--- p5/NavigationGraph.java
+++ p5/NavigationGraph.java
@@ -104,8 +104,85 @@ public class NavigationGraph implements GraphADT<Location, Path> {
 
 	@Override
 	public List<Path> getShortestRoute(Location src, Location dest, String edgePropertyName) {
-		// TODO Auto-generated method stub
-		return null;
+//			boolean visited = false;
+//			double weight = Double.POSITIVE_INFINITY;
+//			Location predecessor = null;
+//			int totalWeight = 0;
+//
+//			PriorityQueue<???> pq = new PriorityQueue<???>(totalWeight, src);
+//			
+//			while(!pq.isEmpty()) {
+//				[C total weight , C] = pq.removeMin();
+//				visited = false;
+//				
+//	            u = pq.Delete_root();
+//	            v = G.nextneighbor(u);
+//
+//	            while(v != -1)  			// for each neighbor of u
+//	            {
+//	            	if(dist[v] > dist[u] + G.edgeLength(u,v)) {
+//	            		dist[v] = dist[u] + G.edgeLength(u,v);
+//				pq.Update(v, dist[v]);
+//	                     }
+//
+//	            	v = G.nextneighbor(u);  	// get the next neighbor of u
+//
+//			}
+//			return null;
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//			   public static void dijkstra_function(graph G, int s) throws IOException
+//	  	   public List<Path> getShortestRoute(Location src, Location dest, String edgePropertyName)
+//			   {                  					// s is the index of the starting vertex
+//			   	// declare variables
+//				int nVerts, u, v;
+//				int [] dist;
+//
+//			        nVerts = G.vertices(); 			// get number of vertices in the graph class
+//
+//			        // initialize array
+//			        dist = new int[nVerts];
+//
+//			        for(v=0; v<nVerts; v++) 			// initializations
+//			        {
+//			        	dist[v] = 99999; 				// 99999 represents infinity
+//
+//			        }// end for
+//
+//			        dist[src] = 0;
+//
+//			        PriorityQueue pq = new PriorityQueue(dist);  		
+//
+//			        while(pq.isEmpty() == 0) 			// if heap is not empty
+//			        {
+//
+//			                u = pq.Delete_root();
+//			                v = G.nextneighbor(u);
+//
+//			                while(v != -1)  			// for each neighbor of u
+//			                {
+//			                	if(dist[v] > dist[u] + G.edgeLength(u,v)) {
+//			                		dist[v] = dist[u] + G.edgeLength(u,v);
+//			
+//									pq.Update(v, dist[v]);
+//			                    }
+//
+//			                	v = G.nextneighbor(u);  	// get the next neighbor of u
+//
+//			                }// end while
+//
+//			        }// end while
+//
+//			        System.out.println("");    			// display the array dist
+//			        for(int row=0; row<nVerts; row++)
+//			      	{
+//			      		System.out.print(dist[row] + " ");
+//			      	  	System.out.println("");
+//			      	}// end for
+//			   }// end bfs_function()
+//			}//end class dijkstra
+//		}
 	}
 
 	@Override
